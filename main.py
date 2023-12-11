063be94d77f65efe74ba83070da69fcfc49f18cf
oscar <oscary324@gmail.com>
2023-12-10 23:06:03 -0500

    tried to add an ai using minimax, it sorta works sometimes, not sure why it doesn't always work, seems to have some type of logical bug somewhere, debugging needed.

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from tkinter import *
-from tkinter import ttk
+from copy import copy
 
 class Game:
-    def __init__(self, root):
-        self.frm = ttk.Frame(root)
+    def __init__(self, root, isAi=False):
+        self.isAi = isAi
+        self.frm = Frame(root, background = "white")
         self.frm.grid()
         self.buttonGrid = [
-            [Button(self.frm, width = 2, height = 1, relief=SUNKEN
-                , font = ("New Times Roman", 80, "bold"))
+            [Button(self.frm, width = 2, height = 0, relief=FLAT
+                , font = ("New Times Roman", 80, "bold")
+                , borderwidth = 0, background = "white"
+                , highlightthickness = 0)
                 for j in range(3)] for i in range(3)]
         for i in range(3):
             for j in range(3):
-                self.buttonGrid[i][j].grid(column = j, row = i)
+                self.buttonGrid[i][j].grid(column = j, row = i, padx=5, pady=5)
                 self.buttonGrid[i][j].config(
                     command = (lambda a,b: lambda: self.move(a,b))(i,j))
         self.reset()
         
     def reset(self):
-        isXmove = True
+        self.isXmove = True
         self.grid = [
             [0, 0, 0],
             [0, 0, 0],
@@ -50,9 +53,12 @@ class Game:
             else:
                 print("Tie! No winner.")
         self.isXmove = not self.isXmove
+        if self.isAi and not self.isXmove:
+            self.move(*self.findNextMove())
 
-    def checkWin(self) -> int:
-        grid = self.grid
+    def checkWin(self, grid=None) -> int:
+        if grid == None:
+            grid = self.grid
         for i in range(3):
             if grid[i][i] == 0:
                 continue
@@ -66,7 +72,54 @@ class Game:
         if self.movesLeft == 0:
             return 4
         return 0
+    
+    def findNextMove(self) -> tuple[int, int]:
+        grid = copy(self.grid)
+        func = None
+        candidate = None
+        xMove = self.isXmove
+        if xMove:
+            candidate = (-1, -1, -1000)
+            func = lambda *x: max(x, key=lambda a: a[2])
+        else:
+            candidate = (-1, -1, 1000)
+            func = lambda *x: min(x, key=lambda a: a[2])
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] != 0: 
+                    print("X", end = " ")
+                    continue
+                grid[i][j] = 1 if xMove else 2
+                candidate = func(candidate, (i, j, (e:=self.minimax(grid,not xMove, not xMove))))
+                grid[i][j] = 0
+                print(e, end = " ")
+            print("")
+        print(candidate)
+        return candidate[:2]
+    
+    def minimax(self, grid, isMin, isXmove) -> int:
+        state = self.checkWin()
+        if state != 0:
+            return (0, 1, -1, 0)[state]
+        func = min if isMin else max
+        candidate = -func(1000, -1000)
+        #print(f"{grid} {candidate} {isMin} ")
+        count = 0
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] != 0:
+                    continue
+                count += 1
+                grid[i][j] = 1 if isXmove else 2
+                candidate = func(candidate, (e:=self.minimax(grid, not isMin, not isXmove)))
+                grid[i][j] = 0
+                #print(e, end=" ")
+            #print("")
+        if count == 0:
+            return 0
+        return candidate
 
 root = Tk()
-game = Game(root)
+root.config(background = "white")
+game = Game(root, True)
 root.mainloop()
